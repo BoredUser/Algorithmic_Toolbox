fe89e2f941d5ef717ee31820fb03c7e0bb245365
BoredUser <iamrahultambe@gmail.com>
2020-06-09 05:48:33 +0530

    Week2 fibonacci number

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,10 +1,9 @@
 # Uses python3
-def calc_fib(n):
-    fib = [0,1]
-    for i in range(n+1):
-        if i>1:
-            fib.append(fib[i - 1] + fib[i - 2])
-    return fib[n]
+def calc_fib(n, computed={0: 0, 1: 1}):
+    if n not in computed:
+        computed[n] = calc_fib(n-1, computed) + calc_fib(n-2, computed)
+    return computed[n]
+
 
 n = int(input())
 print(calc_fib(n))
